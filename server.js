4f47b0e350133af715444889befc93938063c357
Plesh4kov <plesh4kov@gmail.com>
2025-04-08 02:06:57 +0300

    Add socket.io

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
